d95f77fa88debded716ff08231ee26cd0daaee8c
xxlixin1993 <403136170@qq.com>
2018-01-04 11:19:41 +0800

    add Response unit test

--- tests/nats/ResponseTest.php
+++ tests/nats/ResponseTest.php
@@ -6,5 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class ResponseTest extends TestCase
 {
+    /**
+     * Test set and get function
+     */
+    public function testSettingAndGetting()
+    {
+        $options = new \LQueue\nats\ConnectOption();
+        $client = new \LQueue\nats\NatsQueue($options);
+        $client->driver();
+        $response = new \LQueue\nats\Response('foo', 'bar', 'sid', $client);
 
+        $this->assertEquals('foo', $response->getSubject());
+        $this->assertEquals('bar', $response->getBody());
+        $this->assertEquals('bar', $response->__toString());
+        $this->assertEquals('sid', $response->getSid());
+        $this->assertEquals($client, $response->getConn());
+    }
 }
